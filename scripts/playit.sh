f186179fc9b62188f62d4032c302c8dc6d0360bc
kjwolter <kjwolter@pop-os.localdomain>
2022-10-07 23:05:54 -0400

    playit-gg

---
+++ scripts/playit.sh
@@ -0,0 +1,5 @@
+# For Debian & Ubuntu based systems
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
+sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
+sudo apt update
+sudo apt install playit
