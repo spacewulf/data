add08eca0652743a13c7fdaf559ef13924b8bcc6
Kees Wolterstorff <kwolterstorff531@gmail.com>
2024-10-06 23:24:57 -0400

    Updated starship.sh and zoxide.sh to support multiple distros and dynamically add changes to the bash or zsh init files.

--- scripts/starship.sh
+++ scripts/starship.sh
@@ -1,5 +1,29 @@
-curl -sS https://starship.rs/install.sh | sh
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-mkdir ~/.config
-cd ..
-cp configs/starship.toml ~/.config/starship.toml
+#!/bin/bash
+
+curl -sS https://starship.rs/install.sh | sh 
+
+clear
+
+if [ $(echo $SHELL | grep "bash") ];
+then
+	if [[ ! $(cat ~/.bashrc | grep "(starship init bash)") ]];
+	then
+		echo "Adding initialization to your .bashrc file."
+		echo 'eval "$(starship init bash)"' >> ~/.bashrc
+	fi
+elif [ $(echo $SHELL | grep "zsh") ];
+then
+	if [[ ! $(cat ~/.zshrc | grep "(starship init zsh)") ]];
+	then
+		echo "Adding initialization to your .zshrc file."
+		echo 'eval "$(starship init zsh)"'
+	fi
+fi
+
+cd "$(dirname "$0")"
+
+mkdir -p ~/.config
+
+cp ../configs/starship.toml ~/.config/
+
+echo "Copied the starship config file."
--- scripts/zoxide.sh
+++ scripts/zoxide.sh
@@ -1,3 +1,31 @@
-apt install zoxide
-echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
-alias cd=z
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+if [ $(echo $SHELL | grep "bash") ];
+then	
+	if [[ ! $(cat ~/.bashrc | grep "(zoxide init bash)") ]];
+	then
+		echo "Adding initialization to your .bashrc file."
+		echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
+	fi
+	if [[ ! $(cat ~/.bashrc | grep "alias cd=z") ]];
+	then
+		echo "Adding cd -> z aliasing to your .zshrc file."
+		echo 'alias cd=z' >> ~/.bashrc
+	fi
+
+elif [ $(echo $SHELL | grep "zsh") ];
+then
+	if [[ ! $(cat ~/.zshrc | grep "(zoxide init zsh)") ]];
+	then
+		echo "Adding initalization to your .zshrc file."
+		echo 'eval "$(zoxide init zsh)"'
+	fi
+
+	if [[ ! $(cat ~/.zshrc | grep "alias cd=z") ]];
+	then
+		echo "Adding cd -> z aliasing to your .zshrc file."
+		echo 'alias cd=z' >> ~/.zshrc
+	fi
+fi
+
+
